c876e66c9797cbce0e4fb65b7ffe500a9ad80de7
Miguel Velez <miguelvelez@mijecu25.com>
2016-11-05 15:06:49 -0400

    Separate subscribers to a different file

---
+++ src/cp1_gazebo/instructions/localization/monitors.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+import commands
+import rospy
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseArray
+from gazebo_msgs.msg import ModelStates
+from rosgraph_msgs.msg import Clock
+
+CPU_MONITOR_COMMAND = "mpstat 1 1 | grep -o M..all........ | sed -e 's/M  all   //'"
+AMCL_CPU_MONITOR_COMMAND = "pidstat -t -C amcl 1 1 | grep -o .*-..amcl | sed 's/ *- *amcl//' | grep -o '......$'"
+MOVE_BASE_CPU_MONITOR_COMMAND = "pidstat -t -C move_base 1 1 | grep -o .*-..move_base | sed 's/ *- *move_base//' | grep -o '......$'"
+
+
+def gazebo_model_states_callback(data):
+    data_time = rospy.get_rostime().secs
+
+    global gazebo_current_time
+    if gazebo_current_time == data_time:
+        return
+
+    gazebo_current_time = data_time
+    pose = data.pose[2]
+    position = pose.position
+
+    global gazebo_pose_data
+    gazebo_pose_data.append((gazebo_current_time, position.x, position.y))
+
+
+def amcl_pose_callback(data):
+    data_time = data.header.stamp.secs
+
+    global amcl_current_time
+    if amcl_current_time == data_time:
+        return
+
+    amcl_current_time = data_time
+    pose = data.pose.pose
+    position = pose.position
+
+    global amcl_pose_data
+    amcl_pose_data.append((amcl_current_time, position.x, position.y))
+
+
+def clock_proxy_for_cpu_callback(data):
+    data_time = rospy.get_rostime().secs
+
+    global cpu_current_time
+    if cpu_current_time == data_time:
+        return
+
+    cpu_current_time = data_time
+
+    value = commands.getstatusoutput(CPU_MONITOR_COMMAND)[1]
+
+    if len(value) > 0:
+        global cpu_monitor_data
+        cpu_monitor_data.append((cpu_current_time, float(value)))
+
+
+def particlecloud_proxy_for_amcl_cpu_callback(data):
+    data_time = rospy.get_rostime().secs
+
+    global amcl_cpu_current_time
+    if amcl_cpu_current_time == data_time:
+        return
+
+    amcl_cpu_current_time = data_time
+
+    value = commands.getstatusoutput(AMCL_CPU_MONITOR_COMMAND)[1]
+
+    if len(value) > 0:
+        global amcl_cpu_monitor_data
+        amcl_cpu_monitor_data.append((amcl_cpu_current_time, float(value)))
+
+
+if __name__ == '__main__':
+    rospy.init_node('custom_monitors', anonymous=True)
+
+    global gazebo_current_time
+    gazebo_current_time = 0
+    rospy.Subscriber("/gazebo/model_states", ModelStates, gazebo_model_states_callback, queue_size=1)
+
+    global amcl_current_time
+    amcl_current_time = 0
+    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, amcl_pose_callback, queue_size=1)
+
+    global cpu_current_time
+    cpu_current_time = 0
+    rospy.Subscriber("/clock", Clock, clock_proxy_for_cpu_callback, queue_size=1)
+
+    global amcl_cpu_current_time
+    amcl_cpu_current_time = 0
+    rospy.Subscriber("/particlecloud", PoseArray, particlecloud_proxy_for_amcl_cpu_callback, queue_size=1)
+
+    rospy.spin()
--- src/cp1_gazebo/instructions/localization/turtlebot_remote.py
+++ src/cp1_gazebo/instructions/localization/turtlebot_remote.py
@@ -6,14 +6,8 @@ import os
 import shutil
 import re
 import rospy
-import commands
 import socket
 import ConfigParser
-
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import PoseArray
-from gazebo_msgs.msg import ModelStates
-from rosgraph_msgs.msg import Clock
 
 config_parser = ConfigParser.RawConfigParser()
 config_file_path = r'.serverconfig'
@@ -28,120 +22,46 @@ amcl_cpu_monitor_data = []
 move_base_cpu_monitor_data = []
 data_files = {}
 
-GAZEBO_LOG_ERROR = None
-TURTLEBOT_LOG_ERROR = None
+gazebo_log_error = None
+turtlebot_log_error = None
 
 SUCCESS = 'success'
 FAIL = 'fail'
 RESULT = 'result'
 NODE_NAME = 'turtlebot_remote'
-NAVIGATION = 'map_navigation'
-MONITOR_SCAN = 'monitor_scan'
+NAVIGATION = 'map_navigation.py'
+MONITOR_SCAN = 'monitor_scan.py'
+MONITORS_FILE = 'monitors.py'
 DURATION = 'duration'
 GROUND_TRUTH_POSE = 'ground_truth_pose'
 ESTIMATE_POSE = 'estimate_pose'
 CPU_MONITOR = 'cpu_monitor'
 AMCL_CPU_MONITOR = 'amcl_cpu_monitor'
 MOVE_BASE_CPU_MONITOR = 'move_base_cpu_monitor'
-CPU_MONITOR_COMMAND = "mpstat 1 1 | grep -o M..all........ | sed -e 's/M  all   //'"
-AMCL_CPU_MONITOR_COMMAND = "pidstat -t -C amcl 1 1 | grep -o .*-..amcl | sed 's/ *- *amcl//' | grep -o '......$'"
-MOVE_BASE_CPU_MONITOR_COMMAND = "pidstat -t -C move_base 1 1 | grep -o .*-..move_base | sed 's/ *- *move_base//' | grep -o '......$'"
 
 
 def startup(environment_configurations):
     print "Startup"
 
-    global GAZEBO_LOG_ERROR
-    GAZEBO_LOG_ERROR = open('log/gazebo_remote.log', 'a+', 0)
+    global gazebo_log_error
+    gazebo_log_error = open('log/gazebo_remote.log', 'a+', 0)
     subprocess.Popen("./start_remote_gazebo.sh " + str(remote_host) + ' ' + str(remote_password), shell=True,
-                     stderr=GAZEBO_LOG_ERROR, stdout=GAZEBO_LOG_ERROR)
+                     stderr=gazebo_log_error, stdout=gazebo_log_error)
     time.sleep(10)
 
-    global TURTLEBOT_LOG_ERROR
-    TURTLEBOT_LOG_ERROR = open('log/turtlebot_remote.log', 'a+', 0)
-    subprocess.Popen("roslaunch cp1_gazebo robot-altered.launch", shell=True, stderr=TURTLEBOT_LOG_ERROR,
-                     stdout=TURTLEBOT_LOG_ERROR)
+    global turtlebot_log_error
+    turtlebot_log_error = open('log/turtlebot_remote.log', 'a+', 0)
+    subprocess.Popen("roslaunch cp1_gazebo robot-altered.launch", shell=True, stderr=turtlebot_log_error,
+                     stdout=turtlebot_log_error)
     time.sleep(5)
 
-    subprocess.Popen("rosrun cp1_gazebo " + MONITOR_SCAN + '.py "' + str(environment_configurations) + '"', shell=True,
-                     stderr=TURTLEBOT_LOG_ERROR, stdout=TURTLEBOT_LOG_ERROR)
+    subprocess.Popen("rosrun cp1_gazebo " + MONITOR_SCAN + ' "' + str(environment_configurations) + '"', shell=True,
+                     stderr=turtlebot_log_error, stdout=turtlebot_log_error)
     time.sleep(10)
 
     rospy.init_node(NODE_NAME, anonymous=True)
-    global gazebo_current_time
-    gazebo_current_time = 0
-    global amcl_current_time
-    amcl_current_time = 0
-    global cpu_current_time
-    cpu_current_time = 0
-    global amcl_cpu_current_time
-    amcl_cpu_current_time = 0
-    rospy.Subscriber("/gazebo/model_states", ModelStates, gazebo_model_states_callback, queue_size=1)
-    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, amcl_pose_callback, queue_size=1)
-    rospy.Subscriber("/clock", Clock, clock_proxy_for_cpu_callback, queue_size=1)
-    rospy.Subscriber("/particlecloud", PoseArray, particlecloud_proxy_for_amcl_cpu_callback, queue_size=1)
-
-
-def gazebo_model_states_callback(data):
-    data_time = rospy.get_rostime().secs
-
-    global gazebo_current_time
-    if gazebo_current_time == data_time:
-        return
-
-    gazebo_current_time = data_time
-    pose = data.pose[2]
-    position = pose.position
-
-    global gazebo_pose_data
-    gazebo_pose_data.append((gazebo_current_time, position.x, position.y))
-
-
-def amcl_pose_callback(data):
-    data_time = data.header.stamp.secs
-
-    global amcl_current_time
-    if amcl_current_time == data_time:
-        return
-
-    amcl_current_time = data_time
-    pose = data.pose.pose
-    position = pose.position
-
-    global amcl_pose_data
-    amcl_pose_data.append((amcl_current_time, position.x, position.y))
-
-
-def clock_proxy_for_cpu_callback(data):
-    data_time = rospy.get_rostime().secs
-
-    global cpu_current_time
-    if cpu_current_time == data_time:
-        return
-
-    cpu_current_time = data_time
-
-    value = commands.getstatusoutput(CPU_MONITOR_COMMAND)[1]
-
-    if len(value) > 0:
-        global cpu_monitor_data
-        cpu_monitor_data.append((cpu_current_time, float(value)))
-
-
-def particlecloud_proxy_for_amcl_cpu_callback(data):
-    data_time = rospy.get_rostime().secs
-
-    global amcl_cpu_current_time
-    if amcl_cpu_current_time == data_time:
-        return
-
-    amcl_cpu_current_time = data_time
-
-    value = commands.getstatusoutput(AMCL_CPU_MONITOR_COMMAND)[1]
-
-    if len(value) > 0:
-        global amcl_cpu_monitor_data
-        amcl_cpu_monitor_data.append((amcl_cpu_current_time, float(value)))
+    subprocess.Popen("rosrun cp1_gazebo " + MONITORS_FILE, shell=True, stderr=turtlebot_log_error,
+                     stdout=turtlebot_log_error)
 
 
 def shutdown():
@@ -152,17 +72,17 @@ def shutdown():
 
     if not os.path.exists('log/'):
         os.mkdir('log/')
-    global GAZEBO_LOG_ERROR
-    GAZEBO_LOG_ERROR = open('log/gazebo_remote.log', 'a+', 0)
+    global gazebo_log_error
+    gazebo_log_error = open('log/gazebo_remote.log', 'a+', 0)
 
     subprocess.Popen("./stop_remote_roslaunch.sh " + str(remote_host) + ' ' + str(remote_password), shell=True,
-                     stdout=GAZEBO_LOG_ERROR, stderr=GAZEBO_LOG_ERROR)
+                     stdout=gazebo_log_error, stderr=gazebo_log_error)
     time.sleep(20)
 
-    GAZEBO_LOG_ERROR.close()
+    gazebo_log_error.close()
 
-    if TURTLEBOT_LOG_ERROR is not None:
-        TURTLEBOT_LOG_ERROR.close()
+    if turtlebot_log_error is not None:
+        turtlebot_log_error.close()
 
 
 def restart(environment_configurations):
@@ -178,7 +98,7 @@ def measure(id, configurations):
         shutil.rmtree('data/' + str(id))
     os.makedirs('data/' + str(id))
     data_files[NAVIGATION] = open('data/' + str(id) + "/" + NAVIGATION + '.txt', 'a+', 0)
-    subprocess.call("python " + NAVIGATION + '.py "' + str(configurations) + '"', shell=True,
+    subprocess.call("python " + NAVIGATION + ' "' + str(configurations) + '"', shell=True,
                     stdout=data_files[NAVIGATION])
 
     time.sleep(2)
