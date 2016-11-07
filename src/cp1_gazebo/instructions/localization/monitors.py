5bc35267b06a46280471dca2aa9430f1d2cdd9f2
Miguel Velez <miguelvelez@mijecu25.com>
2016-11-07 13:33:41 -0500

    Add another monitor

--- src/cp1_gazebo/instructions/localization/monitors.py
+++ src/cp1_gazebo/instructions/localization/monitors.py
@@ -5,50 +5,21 @@ import commands
 import rospy
 import os
 
-from geometry_msgs.msg import PoseWithCovarianceStamped
+
 from geometry_msgs.msg import PoseArray
-from gazebo_msgs.msg import ModelStates
 from rosgraph_msgs.msg import Clock
 
 DATA_FOLDER = 'monitor_data/'
 MONITORS_FILE = 'monitors_file.txt'
-GROUND_TRUTH_POSE = 'ground_truth_pose'
-ESTIMATE_POSE = 'estimate_pose'
 CPU_MONITOR = 'cpu_monitor'
 AMCL_CPU_MONITOR = 'amcl_cpu_monitor'
-MOVE_BASE_CPU_MONITOR = 'move_base_cpu_monitor'
 
 CPU_MONITOR_COMMAND = "mpstat 1 1 | grep -o M..all........ | sed -e 's/M  all   //'"
 AMCL_CPU_MONITOR_COMMAND = "pidstat -t -C amcl 1 1 | grep -o .*-..amcl | sed 's/ *- *amcl//' | grep -o '......$'"
-MOVE_BASE_CPU_MONITOR_COMMAND = "pidstat -t -C move_base 1 1 | grep -o .*-..move_base | sed 's/ *- *move_base//' | grep -o '......$'"
 
+#
+#
 
-# def gazebo_model_states_callback(data, file):
-#     data_time = rospy.get_rostime().to_sec()
-#
-#     global gazebo_current_time
-#     # if gazebo_current_time == data_time:
-#     #     return
-#
-#     gazebo_current_time = float(data_time)
-#     pose = data.pose[2]
-#     position = pose.position
-#
-#     file.write("time: {} | x: {} | y: {}\n".format(gazebo_current_time, position.x, position.y))
-#
-#
-# def amcl_pose_callback(data, file):
-#     data_time = str(data.header.stamp.secs) + '.' + str(data.header.stamp.nsecs)
-#
-#     # global amcl_current_time
-#     # if amcl_current_time == data_time:
-#     #     return
-#
-#     amcl_current_time = float(data_time)
-#     pose = data.pose.pose
-#     position = pose.position
-#
-#     file.write("time: {} | x: {} | y: {}\n".format(amcl_current_time, position.x, position.y))
 #
 #
 # def clock_proxy_for_cpu_callback(data, file):
@@ -105,29 +76,21 @@ if __name__ == '__main__':
     if not os.path.exists(DATA_FOLDER):
         os.mkdir(DATA_FOLDER)
 
-    monitors = ['ground_truth_pose.py']
+    monitors = ['ground_truth_pose.py', 'estimate_pose.py']
 
     all_monitors_file = open(DATA_FOLDER + MONITORS_FILE, "w", 0)
 
     for monitor in monitors:
-        all_monitors_file.write(monitor.strip('.')[0] + ".txt\n")
+        all_monitors_file.write(monitor.split('.')[0] + ".txt\n")
 
     all_monitors_file.close()
 
     for monitor in monitors:
         subprocess.Popen('python ' + monitor, shell=True)
 
-    # gazebo_current_time = 0
-    # monitor_file = open(DATA_FOLDER + GROUND_TRUTH_POSE + '.txt', "w", 0)
-    # all_monitors_file.write(GROUND_TRUTH_POSE + ".txt\n")
-    # rospy.Subscriber("/gazebo/model_states", ModelStates, callback=gazebo_model_states_callback,
-    #                  callback_args=monitor_file, queue_size=1)
+
     #
-    # amcl_current_time = 0
-    # monitor_file = open(DATA_FOLDER + ESTIMATE_POSE + '.txt', "w", 0)
-    # all_monitors_file.write(ESTIMATE_POSE + ".txt\n")
-    # rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, callback=amcl_pose_callback,
-    #                  callback_args=monitor_file, queue_size=1)
+
     #
     # cpu_current_time = 0
     # monitor_file = open(DATA_FOLDER + CPU_MONITOR + '.txt', "w", 0)
