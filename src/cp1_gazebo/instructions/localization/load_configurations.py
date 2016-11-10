312fde466d6ec2813442f67d1a9d71a50d76f04c
Miguel Velez <miguelvelez@mijecu25.com>
2016-11-10 12:43:25 -0500

    Rename laser to kinect

--- src/cp1_gazebo/instructions/localization/load_configurations.py
+++ src/cp1_gazebo/instructions/localization/load_configurations.py
@@ -228,8 +228,8 @@ amcl_parameters_not_in_website = ['beam_skip_distance', 'beam_skip_threshold', '
                                   'do_beamskip']
 
 
-environment_parameters = [('laser_miscalibration', 0, -0.5, 0.5), ('laser_noise', 0, 0, 1.0)]
-environment_parameters_to_explore = ['laser_miscalibration', 'laser_noise']
+environment_parameters = [('kinect_miscalibration', 0, -0.5, 0.5), ('kinect_noise', 0, 0, 1.0)]
+environment_parameters_to_explore = ['kinect_miscalibration', 'kinect_noise']
 environment_parameters_to_explore_values = [[-0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
                                              0.6, 0.7],
                                             [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
