5b3bface7b55f36be7f1e50783b042755849f3f6
Miguel Velez <miguelvelez@mijecu25.com>
2016-12-09 15:51:03 -0500

    Use id for default configuration

--- src/cp1_gazebo/instructions/localization/default_comparison.R
+++ src/cp1_gazebo/instructions/localization/default_comparison.R
@@ -1,18 +1,21 @@
 # WARNING: This file might not work if the name of the documents do not match the ones we provide here
 
 data_folder = "./data/"
-default_data_files = c('mean_amcl_cpu_utilization_default_data', 'mean_cpu_utilization_default_data',
-                    'mean_localization_error_default_data', 'time_default_data')
+plot_folder = "./plots/"
+default_data_files = c('mean_amcl_cpu_utilization', 'mean_cpu_utilization',
+                    'mean_localization_error', 'time')
+default_options_file = 'e26ab2de-93ae-11e6-bf5b-000c290c1bad_data.csv'
 
 for (i in 1:length(default_data_files)) {
     quartz()
-    matrix = read.csv(paste(data_folder, default_data_files[i], '.csv', sep=''))
+    matrix = read.csv(paste(data_folder, default_data_files[i], '_', default_options_file, sep=''))
 
     invalid_data_indices = matrix > 36
     matrix[invalid_data_indices] = NA
 
     columns = colnames(matrix)
 
+    png(file = paste(plot_folder, 'default_options_file', default_data_files[i], '.png', sep=''), width=1100,height=1100,res=150)
     boxplot(matrix, use.col=TRUE, las=2)
     title(default_data_files[i])
     means = c()
@@ -24,4 +27,6 @@ for (i in 1:length(default_data_files)) {
 
     points(means, col='red')
     abline(h=mean(means), col='red', lwd=3, lty=2)
+
+    dev.off()
 }
