d00ecd2063844470e662048838d1388e6421a743
Miguel Velez <miguelvelez@mijecu25.com>
2017-02-23 14:19:54 -0500

    Deleted submodule

--- src/measurement/instructions/localization/jetway/run_CPU.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-while true
-do
-	mpstat 1 1 | grep -o M..all........ | sed -e 's/M  all   //' >> ~/cpu.log
-done
--- src/measurement/instructions/localization/jetway_package/jetway/sac-compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sac2c nbody/1024_bodies_dynamic.sac -o nbody/tmp.out $@
