df4cbd30407411c6feae4c9ec08f1d8a18400b77
Miguel Velez <miguelvelez@mijecu25.com>
2017-02-25 11:43:41 -0500

    Trying to start ig server

--- src/measurement/localization/move_forward.py
+++ src/measurement/localization/move_forward.py
@@ -22,18 +22,27 @@ def main():
     distance = args.distance
     speed = args.speed
 
+    print distance
+    print speed
+
     rospy.init_node('forward_test')
+    print "rospy.init_node('forward_test')"
     ig = "P(V(1, do Forward({distance},{speed}) then 2),V(2, end)::nil)".format(distance=distance, speed=speed);
+    print "format"
 
     client = actionlib.SimpleActionClient("ig_action_server", ig_action_msgs.msg.InstructionGraphAction)
+    print "get client"
     client.wait_for_server()
+    print "wait for server"
 
     goal = ig_action_msgs.msg.InstructionGraphGoal(order=ig)
+    print "goal"
 
     client.send_goal(goal)
+    print "send goal"
 
     client.wait_for_result()
-
+    print "waited"
 
 if __name__ == "__main__":
     main()
