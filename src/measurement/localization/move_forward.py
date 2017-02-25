8f96ae59201bc1749f77545babf8f974d63887de
Miguel Velez <miguelvelez@mijecu25.com>
2017-02-25 10:59:58 -0500

    Added move forware script

---
+++ src/measurement/localization/move_forward.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import roslib;
+
+roslib.load_manifest('ig_action_msgs')
+import rospy
+
+import actionlib
+import ig_action_msgs.msg
+import sys
+import threading
+
+import requests
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("distance")
+    parser.add_argument("speed")
+    args = parser.parse_args()
+    distance = args.distance
+    speed = args.speed
+
+    rospy.init_node('forward_test')
+    ig = "P(V(1, do Forward({distance},{speed}) then 2),V(2, end)::nil)".format(distance=distance, speed=speed);
+
+    client = actionlib.SimpleActionClient("ig_action_server", ig_action_msgs.msg.InstructionGraphAction)
+    client.wait_for_server()
+
+    goal = ig_action_msgs.msg.InstructionGraphGoal(order=ig)
+
+    client.send_goal(goal)
+
+    client.wait_for_result()
+
+
+if __name__ == "__main__":
+    main()
